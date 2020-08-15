4230bb901f38ba2741b18a61f4938b02c124f43f
daehwan7989@naver.com <daehwan7989@naver.com>
2020-08-15 16:35:03 +0900

    DFS complete

--- src/algorithm/DFS.java
+++ src/algorithm/DFS.java
@@ -3,17 +3,42 @@ package algorithm;
 import java.util.Scanner;
 
 public class DFS {
+	static boolean edge[][];
+	static boolean visited[];
+	static int N;
+	static int M;
+
+	static void dfs(int cur) {
+		visited[cur] = true;
+		System.out.print(cur + " ");
+		for (int i = 1; i <= N; i++) {
+			if (visited[i] || !edge[cur][i])
+				continue;
+			dfs(i);
+		}
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		// 정점의 개수
-		int N = sc.nextInt();
+		N = sc.nextInt();
 
 		// 간선의 개수
-		int M = sc.nextInt();
+		M = sc.nextInt();
+
+		edge = new boolean[N + 1][N + 1];
+		visited = new boolean[N + 1];
 
 		// 시작정점 u, 도착정점 v
 		int u, v;
+
+		for (int i = 0; i < M; i++) {
+			u = sc.nextInt();
+			v = sc.nextInt();
+			edge[u][v] = true;
+		}
+		dfs(1);
 
 	}
 }
